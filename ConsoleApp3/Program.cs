a23e1d24e48e4189ea40ad2426e6056fbbec456c
Каминский Евгений <zhenya.caminscky@yandex.ru>
2021-10-25 19:25:43 +1000

    update 25.10

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,40 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            // Дистанционка, от 25.10
+
+            char array = 'n';
+            Random rnd = new Random();
+
+            while (array == 'n')
+            {
+                int i = rnd.Next(10);
+                Console.WriteLine("Компьютер загадал число от 0 до 10");
+                Console.WriteLine("Введите число");
+
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (i == x)
+                    Console.WriteLine("Вы угадали число");
+                else
+                    Console.WriteLine("Вы не угадали, это число {0}", i);
+                Console.WriteLine("Еще попытку? (n = Да, n = Нет");
+                array = Convert.ToChar(Console.ReadLine());
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
